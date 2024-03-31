563897f3d91bbbadd01e1fb15b5095d11fb43f43
Siddarth Kumar <siddarthkay@gmail.com>
2024-03-31 18:36:01 +0530

    playing with vectors

--- hello.cpp
+++ hello.cpp
@@ -2,13 +2,26 @@
 // Created by Siddarth Kumar on 31/03/24.
 //
 #include <iostream>
+#include <vector>
+#include <string>
 #include "sum.h"
 
 using namespace std;
 using namespace customMath;
 
 int main() {
-    cout << "Hello World!" << "\n";
-    cout << sum(2,4);
+    vector<int> numbersToAdd;
+    vector<string> messageToDisplay;
+
+    numbersToAdd.push_back(2);
+    numbersToAdd.push_back(4);
+
+    messageToDisplay.emplace_back("Hello ");
+    messageToDisplay.emplace_back("World");
+
+    cout << messageToDisplay.at(0) << messageToDisplay.at(1)  << "\n";
+
+    cout << sum(numbersToAdd[0],numbersToAdd[1]);
+
     return 0;
 }
